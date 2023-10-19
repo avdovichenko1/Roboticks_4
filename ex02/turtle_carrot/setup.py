e09c99a7829d2177cf94bf8721b8b26b084e831a
avdovichenko1 <92294006+avdovichenko1@users.noreply.github.com>
2023-10-19 10:48:52 +0700

    Add files via upload

---
+++ ex02/turtle_carrot/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'turtle_carrot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='alina',
+    maintainer_email='alina@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'turtle_tf2_broadcaster = my_carrot.turtle_tf2_broadcaster:main',
+            'turtle_tf2_listener = my_carrot.turtle_tf2_listener:main',
+            'dynamic = my_carrot.dynamic_frame_tf2_broadcaster:main',
+        ],
+    },
+)
